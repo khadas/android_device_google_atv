e40f6d3e5c52ffd51e8d66d89c2e1849e754a4d4
Goenjoy Huang <goenjoy@khadas.com>
2024-08-21 20:07:41 +0800

    add showNavigationBar
    Change-Id: I2fafc59983cf21fc286926825f8676a6aa9fd4e5
    Signed-off-by: Goenjoy Huang <goenjoy@khadas.com>

--- products/atv_system_ext.mk
+++ products/atv_system_ext.mk
@@ -19,9 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_system_ext.mk)
 
 PRODUCT_PACKAGES += \
     blur_sysprop_notifier \
-    TvSystemUI \
-    TvFrameworkPackageStubs \
-    TvSettings
+    TvFrameworkPackageStubs
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/atv/sepolicy/system_ext/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/atv/sepolicy/system_ext/private
